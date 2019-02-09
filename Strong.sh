a16a3390a812f19600c7cde9d0f31367791b592c
Harvey <5703945+RealmOfTibbles@users.noreply.github.com>
2019-02-09 21:39:08 +0000

    Some Bad Code

---
+++ Strong.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+
+function char {
+	case $1 in
+
+		0)
+		echo -n "A";
+		;;
+                1)
+                echo -n "B";
+                ;;
+                2)
+                echo -n "C";
+                ;;
+                3)
+                echo -n "D";
+                ;;
+                4)
+                echo -n "E";
+                ;;
+                5)
+                echo -n "F";
+                ;;
+                6)
+                echo -n "G";
+                ;;
+                7)
+                echo -n "H";
+                ;;
+                8)
+                echo -n "I";
+                ;;
+                9)
+                echo -n "J";
+                ;;
+                10)
+                echo -n "K";
+                ;;
+               	11)
+                echo -n "L";
+                ;;
+                12)
+                echo -n "M";
+                ;;
+                13)
+                echo -n "N";
+                ;;
+                14)
+                echo -n "O";
+                ;;
+                15)
+                echo -n "P";
+                ;;
+                16)
+                echo -n "Q";
+                ;;
+                17)
+                echo -n "R";
+                ;;
+                18)
+                echo -n "S";
+                ;;
+                19)
+                echo -n "T";
+                ;;
+                20)
+                echo -n "U";
+                ;;
+                21)
+                echo -n "V";
+                ;;
+                22)
+                echo -n "W";
+                ;;
+                23)
+                echo -n "X";
+                ;;
+                24)
+                echo -n "Y";
+                ;;
+                25)
+                echo -n "Z";
+                ;;
+                26)
+                echo -n "a";
+                ;;
+                27)
+                echo -n "b";
+                ;;
+                28)
+                echo -n "c";
+                ;;
+                29)
+                echo -n "d";
+                ;;
+                30)
+                echo -n "e";
+                ;;
+                31)
+                echo -n "f";
+                ;;
+                32)
+                echo -n "g";
+                ;;
+                33)
+                echo -n "h";
+                ;;
+                34)
+                echo -n "i";
+                ;;
+                35)
+                echo -n "j";
+                ;;
+                36)
+                echo -n "k";
+                ;;
+                37)
+                echo -n "l";
+                ;;
+                38)
+                echo -n "m";
+                ;;
+                39)
+                echo -n "n";
+                ;;
+                40)
+                echo -n "o";
+                ;;
+                41)
+                echo -n "p";
+                ;;
+                42)
+                echo -n "q";
+                ;;
+                43)
+                echo -n "r";
+                ;;
+                44)
+                echo -n "s";
+                ;;
+                45)
+                echo -n "t";
+                ;;
+                46)
+                echo -n "u";
+                ;;
+                47)
+                echo -n "v";
+                ;;
+                48)
+                echo -n "w";
+                ;;
+                49)
+                echo -n "x";
+                ;;
+                50)
+                echo -n "y";
+                ;;
+                51)
+                echo -n "z";
+                ;;
+                52)
+                echo -n "0";
+                ;;
+                53)
+                echo -n "1";
+                ;;
+                54)
+                echo -n "2";
+                ;;
+                55)
+                echo -n "3";
+                ;;
+                56)
+                echo -n "4";
+                ;;
+                57)
+                echo -n "5";
+                ;;
+                58)
+                echo -n "6";
+                ;;
+                59)
+                echo -n "7";
+                ;;
+                60)
+                echo -n "8";
+                ;;
+                61)
+                echo -n "9";
+                ;;
+                62)
+                echo -n "";
+                ;;
+                63)
+                echo -n "";
+                ;;
+	esac
+};	
+uptime;
+
+for a in `seq 0 63` ;
+do 
+        uptime;
+        for b in `seq 0 63` ;
+                do
+                for c in `seq 0 63` ;
+                        do
+                        for d in `seq 0 63` ;
+                                do 
+                                for e in `seq 0 63` ;
+                                        do
+                                        for f in `seq 0 63` ;
+                                                do
+                                                for g in `seq 0 63` ;
+                                                        do
+                                                        for h in `seq 0 63` ;
+                                                                do
+                                                                for i in `seq 0 63` ;
+                                                                        do 
+                                                                        for j in `seq 0 63` ;
+                                                                                do
+                                                                                for k in `seq 0 63` ;
+                                                                                        do
+                                                                                        for l in `seq 0 63` ;
+                                                                                                do
+                                                                                                for m in `seq 0 63` ;
+                                                                                                        do
+                                                                                                        for n in `seq 0 63` ;
+                                                                                                                do 
+                                                                                                                for o in `seq 0 63` ;
+                                                                                                                        do
+                                                                                                                        for p in `seq 0 63` ;
+                                                                                                                        do
+                                                for q in `seq 0 63` ;
+                                                        do
+                                                        for r in `seq 0 63` ;
+                                                                do
+                                                                for s in `seq 0 63` ;
+                                                                        do 
+                                                                        for t in `seq 0 63` ;
+                                                                                do
+                                                                                
+
+                                                                                echo  ` char $a;char $b;char $c;char $d;char $e; char $f;char $g;char $h;char $i;char $j;char $k;char $l;char $m;char $n;char $o;char $p;char $q;char $r;char $s;char $t;`
+                                done
+                        done
+                done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+uptime;
